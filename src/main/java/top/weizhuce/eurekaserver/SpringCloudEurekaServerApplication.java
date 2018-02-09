c7b91bfcf7f45557c5245da7c3760ec9d41fa8b8
zseapeng <zseapeng@163.com>
2018-02-09 22:28:42 +0800

    Initial commit

---
+++ src/main/java/top/weizhuce/eurekaserver/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package top.weizhuce.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by zseapeng on 2018/02/09
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaServerApplication.class);
+    }
+}
